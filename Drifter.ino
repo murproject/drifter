1848c1cd68a7bc6847fb22c6cbfe7675b8e414cf
zinjkov <zinjkov.su@gmail.com>
2019-10-31 14:05:29 +1000

    some change

--- Drifter.ino
+++ Drifter.ino
@@ -1,76 +1,77 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
 
-  Most Arduinos have an on-board LED you can control. On the Uno and
-  Leonardo, it is attached to digital pin 13. If you're unsure what
-  pin the on-board LED is connected to on your Arduino model, check
-  the documentation at http://arduino.cc
-
-  This example code is in the public domain.
-
-  modified 8 May 2014
-  by Scott Fitzgerald
- */
 #include "TinyGPS.h"
 #include "Http.h"
-// the setup function runs once when you press reset or power the board
+
 TinyGPS gps;
 Http http;
+
+
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+/*!!!Установить const int ID в значение 1, 2 или 3 для каждого дрифтера значение должно быть уникально!!!!*/
+/*!!!!!! т.е. для первого дрифтра установить значение в 1, второго в 2 и для третьего в 3!!!!!!!*/
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+const int ID = 0;
+/* Отображение ID в цвета */
+/* ID = 1 - красный */
+/* ID = 2 - зеленый */
+/* ID = 3 - синий */
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+
 static unsigned long time_update = 0;
+// the setup function runs once when you press reset or power the board
 void setup() {
-	Serial.begin(115200);
-	Serial3.begin(9600);
-	http.init_http();
-	time_update = millis();
+    Serial.begin(115200);
+    if (ID < 1 || ID > 3) {
+        while (true) {
+            Serial.println("/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/");
+            Serial.println("/*!!!!!!!!!!!! WRONG ID !!!!!!!!!!*/");
+            Serial.println("/*!!!!! See line 13 in code! !!!!!*/");
+            Serial.println("/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/");
+            delay(1000);
+        }
+    }
+
+    Serial3.begin(9600);
+    http.init_http();
+    time_update = millis();
 }
 
+
+
+void printgps(float lat, float longt, unsigned long age) {
+    Serial.print("sat: ");
+    Serial.print((int)gps.satellites());
+    Serial.print("lat: ");
+    Serial.print(lat, 7);
+    Serial.print(" long: ");
+    Serial.print(longt, 7);
+    Serial.print(" age: ");
+    Serial.println(age);
+}
+
+
+String toJson(int id, float speed, float lat, float lng) {
+    String format = "{\"id\": \"%1\", \"speed\": \"%2\", \"lat\": \"%3\", \"long\": \"%4\"}";
+    format.replace("%1", String(id));
+    format.replace("%2", String(speed));
+    format.replace("%3", String(lat, 7));
+    format.replace("%4", String(lng, 7));
+    return format;
+}
 // the loop function runs over and over again forever
-
-void printgps(float lat, float longt,  unsigned long age) {
-	Serial.print("sat: ");
-	Serial.print((int)gps.satellites());
-	Serial.print("lat: ");
-	Serial.print(lat);
-	Serial.print(" long: ");
-	Serial.print(longt);
-	Serial.print(" age: ");
-	Serial.println(age);
-}
-
-
-String toJson(float lat, float lon, float alt, float yaw, float sat, float speed, float hdop) {
-	String format = "{\"data\": {\"latitude_gps\": \"%1\", \"longitude_gps\": \"%2\", \"altitude_gps\": \"%3\", \"yaw_gps\": \"%4\",  \"speed_gps\": \"%5\", \"satellites_gps\": \"%6\", \"hdop_gps\": \"%7\"},\"key\": \"around\"}";
-	format.replace("%1", String(lat, 7));
-	format.replace("%2", String(lon, 7));
-	format.replace("%3", String(alt));
-	format.replace("%4", String(yaw));
-	format.replace("%5", String(sat));
-	format.replace("%6", String(speed));
-	format.replace("%7", String(hdop));
-	//Serial.println(format);
-	return format;
-}
-
 void loop() {
-	while (Serial3.available()) {
-
-		if (gps.encode(Serial3.read())) {
-			float lat, longt;
-			unsigned long age;
-			gps.f_get_position(&lat, &longt, &age);
-            String body = toJson(lat, longt, gps.f_altitude(), gps.f_course(), gps.f_speed_kmph(), gps.satellites(), gps.hdop());
-            Serial.println(body);
-            return;
-			Serial.println((millis() - time_update));
-			if ((millis() - time_update) > 30000) {
-				String body = toJson(lat, longt, gps.f_altitude(), gps.f_course(), gps.f_speed_kmph(), gps.satellites(), gps.hdop());
-                Serial.println(body);
-				//http.post("aroundisland.ru/api/board/post/",
-				//	body);
-				time_update = millis();
-			}
-			return;
-		}
-	}
+    while (Serial3.available()) {
+        if (gps.encode(Serial3.read())) {
+            float lat, longt;
+            unsigned long age;
+            gps.f_get_position(&lat, &longt, &age);
+            if ((millis() - time_update) > 6000) {
+                String body = toJson(ID, gps.f_speed_kmph(), lat, longt);
+                http.post("https://robocenter.net/murtracker/point/create/",
+                    body);
+                
+                time_update = millis();
+            }
+        }
+    }
 }
